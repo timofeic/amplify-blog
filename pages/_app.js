1a36ba09db8e7d8d3c7fc4fc35955ad144435209
Tim Cheung <cheungt@amazon.co.uk>
2021-08-28 21:31:06 +0100

    Update _app.js
    Amplify console creates this in the src directory during build time. Change baack to ../aws-exports if we want to deploy this locally.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 import Amplify from 'aws-amplify';
-import config from '../aws-exports';
+import config from '../src/aws-exports';
 Amplify.configure({
   ...config, ssr:true
 });
